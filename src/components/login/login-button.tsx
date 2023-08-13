2f3e6c66554e3f25e3c4e96f01120d56ce0836ea
Maccuci <antoniogabrielleal@gmail.com>
2023-08-13 02:27:56 -0300

    edited login-button component

--- src/components/login/login-button.tsx
+++ src/components/login/login-button.tsx
@@ -2,35 +2,36 @@ import { signIn } from "next-auth/react";
 import GithubIcon from "../icons/github";
 import GoogleIcon from "../icons/google";
 
+const buttonStyles = {
+  base: "mb-2 mr-2 inline-flex items-center rounded-lg px-5 py-2.5 text-center text-sm font-medium transition-all focus:outline-none",
+  github: "bg-[#24292F] text-white hover:-translate-y-0.5 hover:bg-[#24292F]/90 focus:ring-4 focus:ring-[#24292F]/50 dark:hover:bg-[#050708]/30 dark:focus:ring-gray-500",
+  google: "bg-sky-600 text-white hover:-translate-y-0.5 hover:bg-sky-700/90 focus:ring-4 focus:ring-[#4285F4]/50 dark:focus:ring-[#4285F4]/55",
+};
+
+const iconDictionary = {
+  github: <GithubIcon />,
+  google: <GoogleIcon />,
+};
+
 type Props = {
-  type: string;
+  type: "github" | "google";
   callbackUrl: string;
 };
 
 const LoginButton = ({ type, callbackUrl }: Props) => {
+  const signInWithProvider = (provider: string) => {
+    signIn(provider, { callbackUrl });
+  };
+
   return (
-    <div>
-      {type === "github" && (
-        <button
-          type="button"
-          className="mb-2 mr-2 inline-flex items-center rounded-lg bg-[#24292F] px-5 py-2.5 text-center text-sm font-medium text-white transition-all hover:-translate-y-0.5 hover:bg-[#24292F]/90 focus:outline-none focus:ring-4 focus:ring-[#24292F]/50 dark:hover:bg-[#050708]/30 dark:focus:ring-gray-500"
-          onClick={() => signIn("github", { callbackUrl: "/" })}
-        >
-          <GithubIcon />
-          Continuar com Github
-        </button>
-      )}
-      {type === "google" && (
-        <button
-          type="button"
-          className="dark:focus:ring-[#4285F4]/55 mb-2 mr-2 inline-flex items-center rounded-lg bg-sky-600 px-5 py-2.5 text-center text-sm font-medium text-white transition-all hover:-translate-y-0.5 hover:bg-sky-700/90 focus:outline-none focus:ring-4 focus:ring-[#4285F4]/50"
-          onClick={() => signIn("google", { callbackUrl: callbackUrl })}
-        >
-          <GoogleIcon />
-          Continuar com Google
-        </button>
-      )}
-    </div>
+    <button
+      type="button"
+      className={`${buttonStyles.base} ${buttonStyles[type]}`}
+      onClick={() => signInWithProvider(type)}
+    >
+      {iconDictionary[type]}
+      {`Continuar com ${type === "github" ? "Github" : "Google"}`}
+    </button>
   );
 };
 
